471eff07340a2a7e20ab6b87ef5f4fbf8e59ec9a
deepak <dee@dee.com>
2020-02-14 00:49:59 +0530

    fixed the homepage issue

---
+++ precache-manifest.9909abef4eddecdffc4feaa88f23a2ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8986eacc57d1b5ef11877b1ac2693e5",
+    "url": "/DeepaksGarments/index.html"
+  },
+  {
+    "revision": "c828be742cc4bfb6bd01",
+    "url": "/DeepaksGarments/static/css/main.aeef0384.chunk.css"
+  },
+  {
+    "revision": "58d7b40ca7317c9e8196",
+    "url": "/DeepaksGarments/static/js/2.fdcf797b.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/DeepaksGarments/static/js/2.fdcf797b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c828be742cc4bfb6bd01",
+    "url": "/DeepaksGarments/static/js/main.ddda6ba6.chunk.js"
+  },
+  {
+    "revision": "d0a28b913e263236f892",
+    "url": "/DeepaksGarments/static/js/runtime-main.25878166.js"
+  },
+  {
+    "revision": "9eba4e47bd5acb5e5e6f5e18082cf011",
+    "url": "/DeepaksGarments/static/media/horse-logo.9eba4e47.svg"
+  },
+  {
+    "revision": "11ef3e55da8c58d033022aacad281898",
+    "url": "/DeepaksGarments/static/media/shopping-bag.11ef3e55.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/signIn/precache-manifest.ad25998d559aa9b4f06ad18c964d41a4.js"
+  "/DeepaksGarments/precache-manifest.9909abef4eddecdffc4feaa88f23a2ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/signIn/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DeepaksGarments/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
