716b4f814eed696de5ccf3b72a86ff37ee68897c
deepak <dee@dee.com>
2020-02-13 23:59:51 +0530

    build for publish

---
+++ build/precache-manifest.71f4fe3a8c41ed28e5fe1a68fd9f3077.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d62ceb43f4d438f85eadc3d910d001",
+    "url": "/DeepaksGarments/index.html"
+  },
+  {
+    "revision": "7d7ceb80b32252656220",
+    "url": "/DeepaksGarments/static/css/main.a7362222.chunk.css"
+  },
+  {
+    "revision": "58d7b40ca7317c9e8196",
+    "url": "/DeepaksGarments/static/js/2.fdcf797b.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/DeepaksGarments/static/js/2.fdcf797b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d7ceb80b32252656220",
+    "url": "/DeepaksGarments/static/js/main.ddda6ba6.chunk.js"
+  },
+  {
+    "revision": "d0a28b913e263236f892",
+    "url": "/DeepaksGarments/static/js/runtime-main.25878166.js"
+  },
+  {
+    "revision": "9eba4e47bd5acb5e5e6f5e18082cf011",
+    "url": "/DeepaksGarments/static/media/horse-logo.9eba4e47.svg"
+  },
+  {
+    "revision": "11ef3e55da8c58d033022aacad281898",
+    "url": "/DeepaksGarments/static/media/shopping-bag.11ef3e55.svg"
+  }
+]);
